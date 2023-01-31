05d015312987969a84411024dc88d98a9f4a9f47
Sabbirbro <sabbirhossainrifat014@gmail.com>
2023-01-31 21:46:25 +0600

    operation

---
+++ operation.js
@@ -0,0 +1,8 @@
+var ma_dilo = 1000;
+var one_kg_apple = 300;
+var one_kg_orange = 400;
+
+var apple_and_orange_kintekhoroch = one_kg_apple + one_kg_orange;
+
+var tar_kase_baki_thaklo = ma_dilo - apple_and_orange_kintekhoroch;
+console.log(tar_kase_baki_thaklo);
---
+++ shorthand.js
@@ -0,0 +1,17 @@
+var mathematics = 100;
+var bangla = 90;
+var english = 70;
+var social = 60;
+var physics = 87.50;
+var chemistry = 100;
+var islam = 85;
+
+var total = mathematics + bangla + english + social + physics + chemistry + islam;
+
+var gor = total / 7;
+
+var gor = parseFloat(gor).toFixed(2);
+console.log(gor);
+
+
+
